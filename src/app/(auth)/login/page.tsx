821747a29b81ecc09bd6044050f312a9417f7c3f
Ankit Sharma <ankitsharma2615@gmail.com>
2026-03-21 22:53:03 +0530

    add server protected routes using trpc

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,9 @@
 import { LoginForm } from "@/features/auth/components/login-form";
+import { requireUnAuth } from "@/lib/auth-check";
 
-const page = () => {
+const page = async () => {
+  await requireUnAuth();
+
   return <LoginForm />;
 };
 
